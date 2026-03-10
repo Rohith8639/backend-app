3181722df81a9c7dc76c326f46e57c6b5fd60196
Rohith8639 <rohithk00128@gmail.com>
2026-03-10 12:01:02 +0530

    updated

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+import productModel from "../models/productModel.js";
+
+const getProducts = async (req, res) => {
+  const products = await productModel.find();
+  res.render("products/index", { products });
+};
+
+const addProduct=async(req,res)=>{
+    const product=req.body
+    await productModel.create(product);
+    res.redirect("/products");
+};
+const addProductForm=async(req,res)=>{
+    res.render("products/add")
+}
+export {getProducts,addProduct};
---
+++ routes/productRoute.js
@@ -0,0 +1,12 @@
+import {getProducts,addProduct} from "../controllers/productController.js"
+import express from "express"
+
+const productRouter = express.Router()
+
+
+productRouter.get("/",getProducts);
+productRouter.get("/add",addproductForm);
+productRouter.get("")
+
+
+export {productRouter}
